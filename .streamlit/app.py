7b424043f5d84a4c1176d0e4ac1e1bd20c7db70e
yosashafira <yosashafira@gmail.com>
2026-06-25 19:02:24 +0700

    Create app.py

---
+++ .streamlit/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="IIRI Trade Classification",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 IIRI Trade Classification")
+st.write("Aplikasi pemetaan HS → ISIC → Intensitas Teknologi")
